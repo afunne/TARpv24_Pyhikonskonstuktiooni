3c5363bb9baf91d97ef6a09c90f71e0b57043a95
opilane <opilane@HP-CZC2349HTY>
2025-01-20 11:27:17 +0200

    print(), input() funktdioonif 20.01.25

--- Pyhikonskonstuktiooni/Pyhikonskonstuktiooni.py
+++ Pyhikonskonstuktiooni/Pyhikonskonstuktiooni.py
@@ -1,2 +1,50 @@
+from timeit import repeat
+
+
 print("Tere tulemast!", "Tere tulemast! 2" ,end="...", sep="---")
 #Bro, don't forget to revise this in Python courses made by nerds :P
+# andmetüübid
+# täisavr- int, ujukomaarv - float, sõne - str, tõevärtused - bool
+#astendamine **
+print()
+a=5
+aste=3
+tulemus=a**aste
+print(a,"**",aste,"=", tulemus)
+# #korrutamine *
+# arv1=int(input("Esimene korrutaja: "))
+# arv2=int(input("Teine korrutaja: "))
+# tulemus2=arv1*arv2
+# print(arv1,"*",arv2,"=", tulemus)
+#print(type(tulemus))
+#jagamine /
+# arv1=int(input("Esimene korrutaja: "))
+# arv2=int(input("jagaja: "))
+# tulemus=arv1/arv2
+# print(arv1,"/",arv2,"=", tulemus)
+# print(type(tulemus))
+
+# jagamisjääk %
+arv1=int(input("Esimene korrutaja: "))
+arv2=int(input("jagaja: "))
+tulemus=arv1%arv2
+print(arv1,"%",arv2,"=", tulemus)
+print(type(tulemus))
+#liitmine + 
+arv1=int(input("Esimene korrutaja: "))
+arv2=int(input("Teine korrutaja: "))
+tulemus=arv1+arv2
+print(arv1,"+",arv2,"=", tulemus)
+print(type(tulemus))
+#lahutamine -
+arv1=int(input("Esimene korrutaja: "))
+arv2=int(input("Teine korrutaja: "))
+tulemus=arv1-arv2
+print(arv1,"-",arv2,"=", tulemus)
+print(type(tulemus))
+#jagamise täisarvulise osa leidmine //
+arv1=int(input("Esimene korrutaja: "))
+arv2=int(input("jagaja: "))
+tulemus=arv1//arv2
+print(arv1,"//",arv2,"=", tulemus)
+print(type(tulemus))
