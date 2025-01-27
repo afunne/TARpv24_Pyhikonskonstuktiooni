3f2970a93c842d3d569c52afb46b996780f1b528
opilane <opilane@HP-CZC2349HTY>
2025-01-27 09:24:51 +0200

    obama

--- Pyhikonskonstuktiooni/module1.py
+++ Pyhikonskonstuktiooni/module1.py
@@ -41,48 +41,48 @@
 # aastat=int(input("kui vana sa oled?: "))
 # print(f"Tere maailm!, Tervistan sind {nimi}! Sa oled {aastat} aastat vana")
 
-# #this is the second one (who could have guessed?)
-# aastat = 18
-# eesnimi = "Jaak"
-# pikkus = 16.5
-# kas_käib_koolis = True
-# #types
-# print(type(f"Muutaja {aastat} on {type(aastat)}"))
-# print(type(f"Muutaja {eesnimi} on {type(eesnimi)}"))
-# print(type(f"Muutaja {pikkus} on {type(pikkus)}"))
-# print(type(f"Muutaja {kas_käib_koolis} on {type(kas_käib_koolis)}"))
+#this is the second one (who could have guessed?)
+aastat = 18
+eesnimi = "Jaak"
+pikkus = 16.5
+kas_käib_koolis = True
+#types
+print(type(f"Muutaja {aastat} on {type(aastat)}"))
+print(type(f"Muutaja {eesnimi} on {type(eesnimi)}"))
+print(type(f"Muutaja {pikkus} on {type(pikkus)}"))
+print(type(f"Muutaja {kas_käib_koolis} on {type(kas_käib_koolis)}"))
 
-# #third one
-#  #kommid
-# from random import * 
+#third one
+ #kommid
+from random import * 
 
-# kommide_arv=randint(10,100)
-# print(f"Laua peal on {kommide_arv}")
-# mitu=int(input("Mitu tahad süüja"))
-# print(f"Laual peal on jäänud {kommide_arv-mitu}")
+kommide_arv=randint(10,100)
+print(f"Laua peal on {kommide_arv}")
+mitu=int(input("Mitu tahad süüja"))
+print(f"Laual peal on jäänud {kommide_arv-mitu}")
 
-# # 4th one
-# import math
-# puu=float(input("mis on puu diametr?: "))
-# print("vastus on", puu*math.pi)
+# 4th one
+import math
+puu=float(input("mis on puu diametr?: "))
+print("vastus on", puu*math.pi)
 
-# #5th one
-# N=float(input("mis on N?: "))
-# M=float(input("mis on M?: "))
-# print("vastus on", N*M)
+#5th one
+N=float(input("mis on N?: "))
+M=float(input("mis on M?: "))
+print("vastus on", N*M)
 
-# #6th one
-# aeg = int(input("Mitu tundi kulus sõiduks (sekundid)? "))
-# teepikkus = int(input("Mitu kilomeetrit sõitsid (meetrit)? "))
-# print(f"sinu kiirus on: {teepikkus/aeg}")
+#6th one
+aeg = int(input("Mitu tundi kulus sõiduks (sekundid)? "))
+teepikkus = int(input("Mitu kilomeetrit sõitsid (meetrit)? "))
+print(f"sinu kiirus on: {teepikkus/aeg}")
 
-# #7th one
-# esimene_number=float(input("mis on esimene number?: "))
-# teine_number=float(input("mis on teine number?: "))
-# kolmas_number=float(input("mis on kolmas number?: "))
-# neljas_number=float(input("mis on neljas number?: "))
-# vies_number=float(input("mis on vies number?: "))
-# print(f"sinu kiirus on: {esimene_number+teine_number+kolmas_number+neljas_number+vies_number}")
+#7th one
+esimene_number=float(input("mis on esimene number?: "))
+teine_number=float(input("mis on teine number?: "))
+kolmas_number=float(input("mis on kolmas number?: "))
+neljas_number=float(input("mis on neljas number?: "))
+vies_number=float(input("mis on vies number?: "))
+print(f"sinu kiirus on: {esimene_number+teine_number+kolmas_number+neljas_number+vies_number}")
 
 #8th one
 print("  @..@")
