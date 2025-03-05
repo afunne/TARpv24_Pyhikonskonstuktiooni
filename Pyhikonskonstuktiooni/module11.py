fa95bd5663b06e1967950b6ce86b531e71ed780e
opilane <opilane@HP-CZC2349HTY>
2025-03-05 09:59:22 +0200

    end this at home brother

--- Pyhikonskonstuktiooni/module11.py
+++ Pyhikonskonstuktiooni/module11.py
@@ -8,12 +8,55 @@
 #     print()
 
 #ul 1
-SkvorecProV=int(input("Panna koju number: "))
+# try:
+#     SkvorecProV=int(input("Panna koju number: "))
 
-for i in range(SkvorecProV):
-    print(" ----- ")
-    print("|  O  |")
-    print("!  -  !")
-    print(" ----- ")
+#     for i in range(SkvorecProV):
+#         print(" ----- ")
+#         print("|  O  |")
+#         print("!  -  !")
+#         print(" ----- ")
+# except:
+#     print("Nuh uh")
+
+# #ul 2
+# try:
+#     n = int(input("Panna n: "))
+#     stepen = int(input("Panna **"))
+
+#     while True:
+#         koos=n**stepen
+#         if koos > n**3:
+#             print("Proovi uuesti!")
+#             n = int(input("Panna n: "))
+#             stepen = int(input("Panna **"))
+#         else:
+#             print("Täname!")
+#             break
+# except:
+#     print("Nuh uh")
+
+#ul 3
+
+from random import *
+LookImcheesingthis = 0
+for õpilased in randrange(25, 36):
+    print(f"Klassis on {õpilased} inimest.")
+grades = [1, 2, 3, 4, 5]
+while True:
+    for marks in range(õpilased):
+        print(f"See õpilase hind on {grades}.")
+        LookImcheesingthis+=1
+        if LookImcheesingthis == marks:
+            break
+
+
+
+
+
+
+
+
+
 
 
