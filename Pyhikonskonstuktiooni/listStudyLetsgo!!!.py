aafd02a0d592f065cda81dbdff58a343f4e13079
opilane <opilane@HP-CZC2349HTY>
2025-03-10 09:52:48 +0200

    Finish this at home version 2 mr aspir

---
+++ Pyhikonskonstuktiooni/listStudyLetsgo!!!.py
@@ -0,0 +1,61 @@
+# len() - find how much is the leigth in the list
+# isalpha - checks if input is letters
+# isnumeric - checks if input is numbers
+
+from random import *
+sõne="Programmeerimine"
+print(sõne)
+# read by letters
+sõne_list=list(sõne)
+print(sõne_list)
+# reverses
+sõne_list.reverse()
+print(sõne_list)
+# finds input letters
+print(sõne_list.index("P"))
+# how many in a list
+print(len(sõne_list))
+# How many in a word
+print(len(sõne))
+# counts letters in m
+kogus_m=sõne_list.count("m")
+# deletes in m range letters "m"
+for m in range(kogus_m):
+    sõne_list.remove("m")
+# we are inserting numeric in the end of the list
+tähed=randint(1, 6)
+for i in range(tähed):
+    while 1:
+        try:
+            t=input("Sisesta täht: ")
+            if t.isalpha(): break
+        except:
+            print("On vaja täht!")
+    sõne_list.append(input(t))
+print(sõne_list)
+
+
+tähed=randint(1, 6)
+for i in range(tähed):
+    while 1:
+        try:
+            t=input("Sisesta täht: ")
+            if t.isalpha(): break
+        except:
+            print("On vaja täht!")
+    while 1:
+        try:
+            ind=input("Sisesta index: ")
+            if ind.isnumeric() & int(ind)<len(sõne_list): break
+        except:
+            print("On vaja index!")
+    sõne_list.insert(int(ind), t)
+print(sõne_list)
+def function(e):
+    return len(e)
+# sorts the list by the function
+sõne_list.sort(reverse=True, key=function)
+print(sõne_list)
+
+# work that I have to finish at home
+
