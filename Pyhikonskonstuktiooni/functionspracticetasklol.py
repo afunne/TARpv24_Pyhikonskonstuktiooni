34feee6a2cb774aa89535c0b27e7e5d51c2d484b
Hussein Tahmazov <tahmazovhussejn@gmail.com>
2025-03-26 09:58:24 +0200

    Update functionspracticetasklol.py

--- Pyhikonskonstuktiooni/functionspracticetasklol.py
+++ Pyhikonskonstuktiooni/functionspracticetasklol.py
@@ -1,43 +1,120 @@
 from MODULEfunctionspracticetasklol import *
 
 # 1
-tehe, username, password = registreerimine()
-print(f"Kasutajanimi: {username}, Parool: {password}")
+while True:
+    print("Mis võimalik te tahate tegi?")
+    print("""
+    teil on võilikud:
+    1 - registreerimine
+    2 - autoriseerimine
+    3 - nime või parooli muutmine
+    4 - unustanud parooli taastamine
+    """)
 
-# 2
-# https://www.w3schools.com/python/python_dictionaries.asp
-# usersstore = {username: password}
+    try:
+        choice = int(input("valida number: "))
+    except:
+        print("nuh uh")
 
-# print("\n Tere Tulemast! Loggi sisse")
-# checking = autoriseerimine(usersstore)
 
-# if checking:
-#     print("Tere tulemast!")
-# else:
-#     print("Proovi uuesti.")
+    # it is just faster that way
 
-user = [(username, password)]
-autoriseerimine(user)
+    # wantdola = int(input("Kas te tahate jätkata? 1-ja  0-ei"))
+    # if wantdola == 1:
+    #     continue
+    # elif wantdola == 0:
+    #     lõpetamine()
+    #     break
 
-# 3
-# old info
-user = [(username, password)]
-user = muuda_kasutajat(user)
-print(user) 
+    if choice < 0 or choice > 5:
+        print("HUH??? n/ nuh uh n/ Praegu, vali õige võimalikud!")
+        continue
 
-print("Uuendatud kasutajad:", user)
+    if choice == 1:
+        tehe, username, password = registreerimine()
+        print(f"Kasutajanimi: {username}, Parool: {password}")
 
-# 4
-# usersstore = {username: password}
-# usersstore = unu_par_tast(usersstore)
-# print("Uus kasutaja:", usersstore)
-user = [(username, password)]
-user = unu_par_tast(user)
-print(user)
+        wantdola = int(input("Kas te tahate jätkata? 1-ja  0-ei"))
+        if wantdola == 1:
+            continue
+        elif wantdola == 0:
+            lõpetamine()
+            break
+        
 
-# 5
-l�petamine()
+    # 2
+    # https://www.w3schools.com/python/python_dictionaries.asp
+    # usersstore = {username: password}
 
+    # print("\n Tere Tulemast! Loggi sisse")
+    # checking = autoriseerimine(usersstore)
+
+    # if checking:
+    #     print("Tere tulemast!")
+    # else:
+    #     print("Proovi uuesti.")
+
+
+    elif choice == 2:
+        try:
+            user = [(username, password)]
+            autoriseerimine(user)
+
+            wantdola = int(input("Kas te tahate jätkata? 1-ja  0-ei"))
+            if wantdola == 1:
+                continue
+            elif wantdola == 0:
+                lõpetamine()
+                break
+        except:
+            print("Teil pole kasutajanimi")
+            continue
+
+    # 3
+    # old info
+    elif choice == 3:
+        try:
+            user = [(username, password)]
+            user = muuda_kasutajat(user)
+            print(user) 
+
+            print("Uuendatud kasutajad:", user)
+
+            wantdola = int(input("Kas te tahate jätkata? 1-ja  0-ei"))
+            if wantdola == 1:
+                continue
+            elif wantdola == 0:
+                lõpetamine()
+                break
+        except:
+            print("Teil pole kasutajanimi")
+            continue
+
+    # 4
+    # usersstore = {username: password}
+    # usersstore = unu_par_tast(usersstore)
+    # print("Uus kasutaja:", usersstore)
+    elif choice == 4:
+        try:
+            user = [(username, password)]
+            user = unu_par_tast(user)
+            print(user)
+
+            wantdola = int(input("Kas te tahate jätkata? 1-ja  0-ei"))
+            if wantdola == 1:
+                continue
+            elif wantdola == 0:
+                lõpetamine()
+                break
+        except:
+            print("Teil pole kasutajanimi")
+            continue
+
+    # 5
+    # elif choice == 0:
+    #     lõpetamine()
+
+    # is it ironic that 5 is almost everywhere?
 
 
 
