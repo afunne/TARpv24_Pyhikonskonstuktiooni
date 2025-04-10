03fa969aae7bc821af9da0598ce65dcc338bdaf7
opilane <opilane@HP-CZC2349HTY>
2025-04-10 12:45:31 +0300

    I will edn this at home

---
+++ Pyhikonskonstuktiooni/Dictonary.py
@@ -0,0 +1,53 @@
+from module_Dictonary import *
+
+
+
+sonastik = {
+    'koer': 'собака',
+    'kass': 'кошка',
+    'maja': 'дом',
+    'auto': 'машина',
+    'päike': 'солнце'
+}
+
+while True:
+    print("""
+1 - Tõlgi eesti -> vene
+2 - Tõlgi vene -> eesti
+3 - Lisa uus sõna
+4 - Paranda sõna
+5 - Testi teadmisi
+6 - Välju
+0 - lõppimine
+"""
+)
+    try:
+        valik=int(input("Tee on valik: "))
+    except:
+        print("h u h")
+        continue
+
+    if valik == 1:
+        print()
+
+    elif valik == 2:
+        print()
+
+    elif valik == 3:
+        print()
+
+    elif valik == 4:
+        print()
+
+    elif valik == 5:
+        print()
+
+    elif valik == 6:
+        print()
+
+    elif valik == 0:
+        print("Bye, bye! <3 \n*lehvitab käega*")
+        break
+    else:
+        print(".╭╮.")
+        continue
---
+++ Pyhikonskonstuktiooni/module_Dictonary.py
@@ -0,0 +1,14 @@
+# test
+
+# from gtts import gTTS
+# from playsound import playsound
+
+# def raagi(tekst, keel='et'):
+#     obj = gTTS(text=tekst, lang=keel, slow=False)
+#     failinimi = "heli.mp3"
+#     obj.save(failinimi)
+#     playsound(failinimi)
+
+def tolgi_est_rus(sona:dir):
+
+
