f73f55b70c74c8d7c1863488022e50c47bef6246
tahma <tahmazovhussejn@gmail.com>
2025-04-20 21:58:59 +0300

    :_|

--- Pyhikonskonstuktiooni/Dictonary.py
+++ Pyhikonskonstuktiooni/Dictonary.py
@@ -1,6 +1,6 @@
 from module_Dictonary import *
 
-sonastik = lae_sonastik()
+# sonastik = lae_sonastik()
 
 while True:
     print("""
@@ -10,6 +10,8 @@ while True:
 4 - Paranda sõna
 5 - Testi teadmisi
 0 - lõppimine
+(hey, future me is here. I added a bunch of options to debug many things
+I hope nobody minds it)
 """
 )
     try:
@@ -20,25 +22,31 @@ while True:
 
     if valik == 1:
         print()
-        tolgi_est_rus(sonastik)
+        tolgi_est_rus()
     elif valik == 2:
         print()
-        tolgi_rus_est(sonastik)
+        tolgi_rus_est()
 
     elif valik == 3:
         print()
-        lisa_sona(sonastik)
+        lisa_sona()
 
     elif valik == 4:
         print()
-        paranda_sona(sonastik)
+        paranda_sona()
 
     elif valik == 5:
         print()
-        testi_teadmisi(sonastik)
+        testi_teadmisi()
     elif valik == 0:
         print("Bye, bye! <3 \n*lehvitab käega*")
         break
+    elif valik == 6:
+        Loe_failist()
+    elif valik == 7:
+        Kirjuta_failisse
+    elif valik == 8:
+        kuva_sonastik
     else:
         print(".╭╮.")
         continue
--- Pyhikonskonstuktiooni/module_Dictonary.py
+++ Pyhikonskonstuktiooni/module_Dictonary.py
@@ -130,104 +130,296 @@
 #     else:
 #         print("Selge, järgmine kord!")
 
+# import random
+# import os
+
+# # Single file for vocabulary storage
+# VOCAB_FILE = "vocabulary.txt"
+
+# def read_vocabulary() -> dict:
+#     """Read vocabulary from file into dictionary (Estonian-Russian)"""
+#     vocabulary = {}
+#     try:
+#         with open(VOCAB_FILE, 'r', encoding='utf-8-sig') as f:
+#             for line in f:
+#                 if '-' in line:
+#                     est, rus = line.strip().split('-')
+#                     vocabulary[est] = rus
+#     except FileNotFoundError:
+#         print("Vocabulary file not found. Creating new one.")
+#         with open(VOCAB_FILE, 'w', encoding='utf-8-sig') as f:
+#             pass
+#     return vocabulary
+
+# def write_vocabulary(vocabulary: dict):
+#     """Write dictionary to vocabulary file"""
+#     with open(VOCAB_FILE, 'w', encoding='utf-8-sig') as f:
+#         for est, rus in vocabulary.items():
+#             f.write(f"{est}-{rus}\n")
+
+# def translate_est_to_rus():
+#     """Translate from Estonian to Russian"""
+#     vocabulary = read_vocabulary()
+#     word = input("Sisesta eesti keelne sõna: ").lower()
+#     translation = vocabulary.get(word, "Sõna sõnastikus puudub")
+#     print(f"Tõlge: {translation}")
+
+# def translate_rus_to_est():
+#     """Translate from Russian to Estonian"""
+#     vocabulary = read_vocabulary()
+#     word = input("Sisesta vene keelne sõna: ").lower()
+#     # Create reverse dictionary for lookup
+#     rus_est = {rus: est for est, rus in vocabulary.items()}
+#     translation = rus_est.get(word, "Sõna sõnastikus puudub")
+#     print(f"Tõlge: {translation}")
+
+# def add_word():
+#     """Add new word to vocabulary"""
+#     est_word = input("Sisesta eesti keelne sõna: ").lower()
+#     rus_word = input("Sisesta vene tõlge: ").lower()
+    
+#     vocabulary = read_vocabulary()
+#     vocabulary[est_word] = rus_word
+#     write_vocabulary(vocabulary)
+    
+#     print(f"Sõna '{est_word}' lisatud sõnastikku!")
+
+# def correct_word():
+#     """Correct existing word in vocabulary"""
+#     vocabulary = read_vocabulary()
+#     est_word = input("Sisesta parandatav eesti keelne sõna: ").lower()
+    
+#     if est_word in vocabulary:
+#         new_rus = input(f"Sisesta uus vene tõlge sõnale '{est_word}': ").lower()
+#         vocabulary[est_word] = new_rus
+#         write_vocabulary(vocabulary)
+#         print("Sõna parandatud!")
+#     else:
+#         print("Sellist sõna sõnastikus pole.")
+
+# def test_knowledge():
+#     """Test user's vocabulary knowledge"""
+#     vocabulary = read_vocabulary()
+#     if not vocabulary:
+#         print("Sõnastik on tühi. Palun lisa esmalt sõnu.")
+#         return
+    
+#     correct = 0
+#     total = 0
+    
+#     confirm = input("Kas te tahate testida? (jah/ei): ").lower()
+#     if confirm == 'jah':
+#         while True:
+#             test_type = input("\nVali testitüüp: vene - 1, eesti - 2, lõpetamiseks - 0: ")
+            
+#             if test_type == '1':
+#                 # Test Estonian to Russian
+#                 est_word = random.choice(list(vocabulary.keys()))
+#                 correct_translation = vocabulary[est_word]
+#                 user_answer = input(f"Kuidas tõlgitakse '{est_word}' vene keelde? ").lower()
+#                 total += 1
+#                 if user_answer == correct_translation:
+#                     print("Õige!")
+#                     correct += 1
+#                 else:
+#                     print(f"Vale! Õige vastus on '{correct_translation}'")
+            
+#             elif test_type == '2':
+#                 # Test Russian to Estonian
+#                 rus_word = random.choice(list(vocabulary.values()))
+#                 # Find corresponding Estonian word
+#                 est_word = None
+#                 for est, rus in vocabulary.items():
+#                     if rus == rus_word:
+#                         est_word = est
+#                         break
+    
+#                 if est_word is None:
+#                     print("lol")
+#                     continue
+#                 user_answer = input(f"Kuidas tõlgitakse '{rus_word}' eesti keelde? ").lower()
+#                 total += 1
+#                 if user_answer == est_word:
+#                     print("Õige!")
+#                     correct += 1
+#                 else:
+#                     print(f"Vale! Õige vastus on '{est_word}'")
+            
+#             elif test_type == '0':
+#                 if total > 0:
+#                     percentage = (correct / total) * 100
+#                     print(f"\nTest lõppenud! Õiged vastused: {correct}, valed: {total - correct}")
+#                     print(f"Protsent: {percentage:.2f}%")
+#                 else:
+#                     print("Testi ei tehtud.")
+#                 break
+#             else:
+#                 print("Vigane sisestus, proovi uuesti.")
+#     else:
+#         print("Selge, järgmine kord!")
+
+# def display_vocabulary():
+#     """Show all words in vocabulary"""
+#     vocabulary = read_vocabulary()
+#     if not vocabulary:
+#         print("Sõnastik on tühi.")
+#         return
+    
+#     # for debuging purposes
+#     print("\n=== SÕNASTIK ===")
+#     for i, (est, rus) in enumerate(vocabulary.items(), 1):
+#         print(f"{i}. {est} - {rus}")
+
+
 import random
-import os
 
-DICTIONARY_FILE = "sonastik.txt"
-
-def lae_sonastik():
+def Loe_failist(fail:str)->list:
     """
-    Laeb sõnastiku failist sõnastikku (dict)
+    Faili lugemine ridade loeteluks
     """
-    sona = {}
-    if os.path.exists(DICTIONARY_FILE):
-        with open(DICTIONARY_FILE, "r", encoding="utf-8") as f:
-            for line in f:
-                if ':' in line:
-                    est, rus = line.strip().split(":", 1)
-                    sona[est.strip()] = rus.strip()
-    return sona
+    try:
+        with open(fail, 'r', encoding="utf-8-sig") as f:
+            return [rida.strip() for rida in f]
+    except FileNotFoundError:
+        print(f"Faili {fail} ei leitud!")
+        return
 
-def salvesta_sonastik(sonastik):
+def tolgi_est_rus():
     """
-    Salvestab sõnastiku faili
+    Tõlge eesti keelest vene keelde tekstifailist
     """
-    with open(DICTIONARY_FILE, "w", encoding="utf-8") as f:
-        for est, rus in sonastik.items():
-            f.write(f"{est}:{rus}\n")
-
-def tolgi_est_rus(sonastik):
-    word = input("Sisesta eesti keelne sõna: ").lower()
-    translation = sonastik.get(word, "Sõna sõnastikus puudub")
-    print(f"Tõlge: {translation}")
-
-def tolgi_rus_est(sonastik):
-    word = input("Sisesta vene keelne sõna: ").lower()
-    translation = "Sõna sõnastikus puudub"
-    for est, rus in sonastik.items():
-        if rus == word:
-            translation = est
-            break
-    print(f"Tõlge: {translation}")
-
-def lisa_sona(sonastik):
-    est_sona = input("Sisesta eesti keelne sõna: ").lower()
-    rus_sona = input("Sisesta vene tõlge: ").lower()
-    sonastik[est_sona] = rus_sona
-    salvesta_sonastik(sonastik)
-    print(f"Sõna '{est_sona}' lisatud sõnastikku!")
-
-def paranda_sona(sonastik):
-    est_sona = input("Sisesta parandatav eesti keelne sõna: ").lower()
-    if est_sona in sonastik:
-        new_rus = input(f"Sisesta uus vene tõlge sõnale '{est_sona}': ").lower()
-        sonastik[est_sona] = new_rus
-        salvesta_sonastik(sonastik)
-        print("Sõna parandatud!")
-    else:
-        print("Sellist sõna sõnastikus pole.")
-
-def testi_teadmisi(sonastik):
-    correct = 0
-    total = 0
-
-    confirm = int(input("Kas te tahate testida? 1 - jah, 0 - ei: "))
-    if confirm == 1:
-        while True:
-            testlvl = int(input("\nVali testitüüp: vene - 1, eesti - 2, lõpetamiseks - 0: "))
-            if testlvl == 1:
-                est = random.choice(list(sonastik))
-                rus = sonastik[est]
-                print("\nTestime eesti-vene suunal:")
-                useranswer = input(f"Kuidas tõlgitakse '{est}' vene keelde? ").lower()
-                total += 1
-                if useranswer == rus:
-                    print("Õige!")
-                    correct += 1
-                else:
-                    print(f"Vale! Õige vastus on '{rus}'")
-
-            elif testlvl == 2:
-                rus = random.choice(list(sonastik.values()))
-                est = [k for k, v in sonastik.items() if v == rus][0]
-                print("\nTestime vene-eesti suunal:")
-                useranswer = input(f"Kuidas tõlgitakse '{rus}' eesti keelde? ").lower()
-                total += 1
-                if useranswer == est:
-                    print("Õige!")
-                    correct += 1
-                else:
-                    print(f"Vale! Õige vastus on '{est}'")
-
-            elif testlvl == 0:
-                if total > 0:
-                    result = (correct / total) * 100
-                    print(f"\nTest lõppenud! Õiged vastused: {correct}, valed: {total - correct}")
-                    print(f"Protsent: {result:.1f}%")
-                else:
-                    print("Testi ei tehtud.")
+    sonad = Loe_failist("sonastik.txt")
+    if not sonad:
+        print("Sõnastik on tühi!")
+        return
+    
+    otsitav = input("Sisesta eesti keelne sõna: ").lower()
+    leitud = False
+    
+    for paar in sonad:
+        if '-' in paar:
+            est, rus = paar.split('-')
+            if est == otsitav:
+                print(f"Tõlge: {rus}")
+                leitud = True
                 break
-            else:
-                print("Vigane sisestus, proovi uuesti.")
+    
+    if not leitud:
+        print("Sõna ei leitud sõnastikust!")
+
+def tolgi_rus_est():
+    """
+    Tõlge vene keelest eesti keelde tekstifailist
+    """
+    sonad = Loe_failist("sonastik.txt")
+    if not sonad:
+        print("Sõnastik on tühi!")
+        return
+    
+    otsitav = input("Sisesta vene keelne sõna: ").lower()
+    leitud = False
+    
+    for paar in sonad:
+        if '-' in paar:
+            est, rus = paar.split('-')
+            if rus == otsitav:
+                print(f"Tõlge: {est}")
+                leitud = True
+                break
+    
+    if not leitud:
+        print("Sõna ei leitud sõnastikust!")
+
+def Kirjuta_failisse(fail:str, jarjend:list):
+    """
+    Nimekirja kirjutamine faili
+    """
+    with open(fail, 'w', encoding="utf-8-sig") as f:
+        f.write('\n'.join(jarjend))
+
+def kuva_sonastik():
+    """
+    Kuvada kõik sõnad sõnavaras
+    """
+    sonad = Loe_failist("sonastik.txt")
+    if not sonad:
+        print("Sõnastik on tühi!")
     else:
-        print("Selge, järgmine kord!")
+        print("\nSõnastik:")
+        for i, paar in enumerate(sonad, 1):
+            print(f"{i}. {paar.replace('-', ' - ')}")
+
+def lisa_sona():
+    """
+    Lisa uus sõnapaar
+    """
+    est = input("Eesti sõna: ").lower()
+    rus = input("Vene tõlge: ").lower()
+    sonad = Loe_failist("sonastik.txt")
+    sonad.append(f"{est}-{rus}")
+    Kirjuta_failisse("sonastik.txt", sonad)
+    print(f"'{est}' lisatud!")
+
+def paranda_sona():
+    """
+    Olemasoleva sõna muutmine
+    """
+    sonad = Loe_failist("sonastik.txt")
+    kuva_sonastik()
+    try:
+        nr = int(input("Mitmes sõna parandada? ")) - 1
+        if 0 <= nr < len(sonad):
+            est, rus = sonad[nr].split('-')
+            uus_rus = input(f"Uus tõlge sõnale '{est}': ")
+            sonad[nr] = f"{est}-{uus_rus}"
+            Kirjuta_failisse("sonastik.txt", sonad)
+            print("Parandatud!")
+        else:
+            print("Vale number!")
+    except:
+        print("Viga!")
+
+def testi_teadmisi():
+    """
+    Sõnavara test
+    """
+    sonad = Loe_failist("sonastik.txt")
+    if not sonad:
+        print("Sõnastik on tühi!")
+        return
+    
+    õiged = 0
+    küsimused = 0
+    
+    while True:
+        suund = input("\nTesti suund (1-est>rus, 2-rus>est, 0-katkesta): ")
+        
+        if suund == '1':
+            paar = random.choice(sonad)
+            est, rus = paar.split('-')
+            vastus = input(f"Kuidas tõlgitakse '{est}'? ").lower()
+            if vastus == rus:
+                print("Õige!")
+                õiged += 1
+            else:
+                print(f"Vale! Õige: {rus}")
+            küsimused += 1
+            
+        elif suund == '2':
+            paar = random.choice(sonad)
+            est, rus = paar.split('-')
+            vastus = input(f"Kuidas tõlgitakse '{rus}'? ").lower()
+            if vastus == est:
+                print("Õige!")
+                õiged += 1
+            else:
+                print(f"Vale! Õige: {est}")
+            küsimused += 1
+            
+        elif suund == '0':
+            if küsimused > 0:
+                print(f"\nTulemus: {õiged}/{küsimused} ({(õiged/küsimused)*100:.1f}%)")
+            break
+        
+        else:
+            print("Vale valik!")
